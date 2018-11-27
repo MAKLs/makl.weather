e7e1c69ff21c94a4324498508351b65fe412505b
MAKLs <mikeashimko@gmail.com>
2018-11-27 06:57:14 -0500

    Move Get-Weather.ps1

--- public/Get-Weather.ps1
+++ public/Get-Weather.ps1
@@ -8,21 +8,44 @@ enum Units
 
 function Get-Weather
 {
+	[cmdletbinding(DefaultParameterSetName = "coord")]
 	param
 	(
+		[Parameter(ParameterSetName = "coord")]
+		[array]$Coordinates = @(),
+		
+		[Parameter(ParameterSetName = "city")]
 		[string]$City="Philadelphia",
+		
+		[Parameter(ParameterSetName = "city")]
 		[string]$CountryCode="US",
 		
+		[Parameter(ParameterSetName = "coord")]
+		[Parameter(ParameterSetName = "city")]
 		[Units[]]
 		$Unit=[Units]::imperial,
 		
+		[Parameter(ParameterSetName = "coord")]
+		[Parameter(ParameterSetName = "city")]
 		[switch]$PassThru
 	)
-	
+
+	$endpoint = $Global:SETTINGS.api.weather.endpoint
+	$key = $Global:SETTINGS.api.weather.key
 	$unitVal = if ($Unit -ne [Units]::kelvin) {"&units={0}" -f ($Unit -as [string])} else {""}
-	$endpoint = $Global:SETTINGS.api.endpoint
-	$key = $Global:SETTINGS.api.key
-	$query = "?q={0},{1}{2}" -f $City,$CountryCode,$unitVal
+	if ($PSCmdlet.ParameterSetName -eq "coord")
+	{
+		if ($Coordinates.Count -ne 2)
+		{
+			$Coordinates = Get-MyLocation
+			$Coordinates = @($Coordinates.lat, $Coordinates.lon)
+		}
+		$query = "?lat={0}&lon={1}{2}" -f ($Coordinates + ,$unitVal)
+	}
+	elseif ($PSCmdlet.ParameterSetName -eq "city")
+	{
+		$query = "?q={0},{1}{2}" -f $City,$CountryCode,$unitVal
+	}
 	$request = $endpoint + $query + ("&APPID={0}" -f $key)
 	
 	try
