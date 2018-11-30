2351861f09bbe9a5da120b189cd766d036945640
MAKLs <mikeashimko@gmail.com>
2018-11-30 18:04:28 -0500

    Adjust temperature gradient limits

--- private/Set-DatumColor.ps1
+++ private/Set-DatumColor.ps1
@@ -13,7 +13,7 @@ function Set-DatumColor
    $valueKelvin = ConvertTo-Kelvin -DatumType $DatumType -Value $Value -SourceUnit $SourceUnit
    switch ($DatumType)
    {
-      ([DatumTypes]::Temperature)   {$gradparams = @{Lo = 273; Mid = $null; Hi = 300}; break}
+      ([DatumTypes]::Temperature)   {$gradparams = @{Lo = 266; Mid = 289; Hi = 310}; break}
       ([DatumTypes]::WindSpeed)     {$gradparams = @{Lo = 3; Mid = 6; Hi = 15; Gradient = @{Lo = @(145,255,0); Mid = @(255,255,51); Hi = @(255,40,0)}}; break}
       default                       {$gradparams = @{Lo = $valueKelvin; Mid = $valueKelvin; Hi = $valueKelvin}}
    }
