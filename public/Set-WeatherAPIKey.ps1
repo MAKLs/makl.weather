9074c15471b0191dcf60efba5bc85ee4e977b4b7
MAKLs <mikeashimko@gmail.com>
2018-12-01 15:41:39 -0500

    Prompt user whether API key should be saved to disk

--- public/Set-WeatherAPIKey.ps1
+++ public/Set-WeatherAPIKey.ps1
@@ -7,10 +7,35 @@ function Set-WeatherAPIKey
 
    $moduleRoot = Split-Path $PSScriptRoot -Parent
 
-   #Save the API Key change
+   #Change the API key
    $Global:WEATHER_SETTINGS.settings.api.weather.key = $APIKey
-   $Global:WEATHER_SETTINGS.Save((Join-Path $moduleRoot "settings.xml"))
 
-   #Re-import module for change to take effect
-   Import-Module (Join-Path $moduleRoot "makl.weather.psm1") -Force -Global | Out-Null
+   #Save the change to disk
+   $checking = $true
+   $positiveResponses = [ValidateSet]::new("y","yes","yeah","yep")
+   $negativeResponses = [ValidateSet]::new("n","no","nah","nope")
+   while ($checking)
+   {
+      switch (Read-Host "Are you sure you'd like to permanently change your API key to ${APIKey}? (y/n)")
+      {
+         {$positiveResponses.ValidValues.Contains($_)}
+         {
+            Write-Host "Saving API key to disk for later sessions" -ForegroundColor Green
+            $Global:WEATHER_SETTINGS.Save((Join-Path $moduleRoot "settings.xml"))
+            $checking = $false
+            break
+         }
+         {$negativeResponses.ValidValues.Contains($_)}
+         {
+            Write-Warning "API key change will only be in effect for this session"
+            $checking = $false
+            break
+         }
+         default
+         {
+            Write-Host "Invalid response. Please enter 'y' or 'n'"
+         }
+      }
+
+   }
 }
